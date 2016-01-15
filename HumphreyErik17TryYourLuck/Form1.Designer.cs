3689c19943e586cfa2988fa212d8c8d5d9ba3d38
Erik Humphrey <ContactErikH@gmail.com>
2016-01-15 13:12:08 -0500

    final commit basically

---
+++ HumphreyErik17TryYourLuck/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace HumphreyErik17TryYourLuck
+{
+    partial class frmTryYourLuck
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTryYourLuck));
+            this.lstGameLog = new System.Windows.Forms.ListBox();
+            this.btnRollDice = new System.Windows.Forms.Button();
+            this.lblCounterWins = new System.Windows.Forms.Label();
+            this.lblCounterLosses = new System.Windows.Forms.Label();
+            this.lblWinLossPercentage = new System.Windows.Forms.Label();
+            this.btnResetGame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstGameLog
+            // 
+            this.lstGameLog.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lstGameLog.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstGameLog.FormattingEnabled = true;
+            this.lstGameLog.ItemHeight = 18;
+            this.lstGameLog.Location = new System.Drawing.Point(0, 87);
+            this.lstGameLog.Name = "lstGameLog";
+            this.lstGameLog.Size = new System.Drawing.Size(398, 310);
+            this.lstGameLog.TabIndex = 0;
+            // 
+            // btnRollDice
+            // 
+            this.btnRollDice.AutoSize = true;
+            this.btnRollDice.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnRollDice.Font = new System.Drawing.Font("Myriad Web Pro", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRollDice.Image = global::HumphreyErik17TryYourLuck.Properties.Resources.imgDieSmall;
+            this.btnRollDice.Location = new System.Drawing.Point(12, 4);
+            this.btnRollDice.Name = "btnRollDice";
+            this.btnRollDice.Size = new System.Drawing.Size(76, 79);
+            this.btnRollDice.TabIndex = 1;
+            this.btnRollDice.UseVisualStyleBackColor = true;
+            this.btnRollDice.Click += new System.EventHandler(this.btnRollDice_Click);
+            // 
+            // lblCounterWins
+            // 
+            this.lblCounterWins.AutoSize = true;
+            this.lblCounterWins.Font = new System.Drawing.Font("Myriad Web Pro Condensed", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCounterWins.Location = new System.Drawing.Point(94, 19);
+            this.lblCounterWins.Name = "lblCounterWins";
+            this.lblCounterWins.Size = new System.Drawing.Size(89, 30);
+            this.lblCounterWins.TabIndex = 2;
+            this.lblCounterWins.Text = "Wins: 0";
+            // 
+            // lblCounterLosses
+            // 
+            this.lblCounterLosses.AutoSize = true;
+            this.lblCounterLosses.Font = new System.Drawing.Font("Myriad Web Pro Condensed", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCounterLosses.Location = new System.Drawing.Point(94, 49);
+            this.lblCounterLosses.Name = "lblCounterLosses";
+            this.lblCounterLosses.Size = new System.Drawing.Size(106, 30);
+            this.lblCounterLosses.TabIndex = 4;
+            this.lblCounterLosses.Text = "Losses: 0";
+            // 
+            // lblWinLossPercentage
+            // 
+            this.lblWinLossPercentage.AutoSize = true;
+            this.lblWinLossPercentage.Font = new System.Drawing.Font("Myriad Web Pro Condensed", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWinLossPercentage.Location = new System.Drawing.Point(259, 19);
+            this.lblWinLossPercentage.Name = "lblWinLossPercentage";
+            this.lblWinLossPercentage.Size = new System.Drawing.Size(111, 30);
+            this.lblWinLossPercentage.TabIndex = 5;
+            this.lblWinLossPercentage.Text = "W / L: 0%";
+            // 
+            // btnResetGame
+            // 
+            this.btnResetGame.Font = new System.Drawing.Font("Myriad Web Pro", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResetGame.Location = new System.Drawing.Point(264, 49);
+            this.btnResetGame.Name = "btnResetGame";
+            this.btnResetGame.Size = new System.Drawing.Size(124, 30);
+            this.btnResetGame.TabIndex = 6;
+            this.btnResetGame.Text = "Reset game";
+            this.btnResetGame.UseVisualStyleBackColor = true;
+            this.btnResetGame.Click += new System.EventHandler(this.btnResetGame_Click);
+            // 
+            // frmTryYourLuck
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 397);
+            this.Controls.Add(this.btnResetGame);
+            this.Controls.Add(this.lblWinLossPercentage);
+            this.Controls.Add(this.lblCounterLosses);
+            this.Controls.Add(this.lblCounterWins);
+            this.Controls.Add(this.btnRollDice);
+            this.Controls.Add(this.lstGameLog);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmTryYourLuck";
+            this.Text = "Try Your Luck";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstGameLog;
+        private System.Windows.Forms.Button btnRollDice;
+        private System.Windows.Forms.Label lblCounterWins;
+        private System.Windows.Forms.Label lblCounterLosses;
+        private System.Windows.Forms.Label lblWinLossPercentage;
+        private System.Windows.Forms.Button btnResetGame;
+    }
+}
+
---
+++ HumphreyErik17TryYourLuck/Form1.cs
@@ -0,0 +1,128 @@
+﻿/* _____           __   __                 _                _    
+  |_   _|          \ \ / /                | |              | |   
+    | |_ __ _   _   \ V /___  _   _ _ __  | |    _   _  ___| | __
+    | | '__| | | |   \ // _ \| | | | '__| | |   | | | |/ __| |/ /
+    | | |  | |_| |   | | (_) | |_| | |    | |___| |_| | (__|   < 
+    \_/_|   \__, |   \_/\___/ \__,_|_|    \_____/\__,_|\___|_|\_\
+             __/ |                                               
+            |___/                                                 */
+
+/* Author: Erik Humphrey
+ * Project title: Project 17 - Try Your Luck
+ * Date started: December 3rd
+ * Executable name: HumphreyErik17TryYourLuck.exe
+ * Description: Single-button dice-rolling simulation 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HumphreyErik17TryYourLuck
+{
+    public partial class frmTryYourLuck : Form
+    {
+        // Global declarations
+        int dice1, dice2, diceTotal, gamesPlayed, wins, losses;
+        Random rnd = new Random();
+
+        public frmTryYourLuck()
+        {
+            InitializeComponent();
+        }
+
+        // Method to clear the game log
+
+        void ClearLog()
+        {
+            lstGameLog.Items.Clear();
+        }
+
+        // User clicks roll dice button
+
+        private void btnRollDice_Click(object sender, EventArgs e)
+        {
+            btnResetGame.Enabled = true; // Enable `Reset winrate` button
+            bool lost = false; 
+            ClearLog(); // Clear the game log ListBox
+
+            do
+            {
+                // Give the dice variables random values from 1-6
+                dice1 = rnd.Next(1, 7);
+                dice2 = rnd.Next(1, 7);
+                // Total the values of the dice
+                diceTotal = dice1 + dice2;
+                lstGameLog.Items.Add("You roll a " + dice1 + " and a " + dice2 + "."); // Print values rolled to game log
+                // If dice values are equal, set that the user has lost
+                if (dice1 == dice2)
+                {
+                    lost = true;
+                }
+            }
+            while (diceTotal != 7 && lost == false); // repeat the above as long as the user has not lost or rolled a total of 7 (a win)
+             
+            float winPercent = 0;
+
+            // If the user lost
+            if (lost == true)
+            {
+                lstGameLog.Items.Add("You lost."); // Print result to game log
+                losses++; // Increase loss amount
+                gamesPlayed = losses + wins;
+                lblCounterLosses.Text = "Losses: " + losses; // Update the label counting losses 
+                // Calculate win percentage 
+                if (gamesPlayed > 0)
+                    winPercent = ((float)wins / (float)gamesPlayed) * 100;
+                lblWinLossPercentage.Text = "W / L: " + (int)winPercent + "%"; // Update W/L label
+
+            }
+            // If the user won
+            else
+            {
+                lstGameLog.Items.Add("You win!");
+                wins++;
+                gamesPlayed = losses + wins;
+                lblCounterWins.Text = "Wins: " + wins;
+                if (gamesPlayed > 0)
+                    winPercent = ((float)wins / (float)gamesPlayed * 100);
+                lblWinLossPercentage.Text = "W / L: " + (int)winPercent + "%";
+            }
+
+            // Bonus message in game log for milestones in games played
+            if ( gamesPlayed == 10  ||
+                 gamesPlayed == 25  ||
+                 gamesPlayed == 50  ||
+                 gamesPlayed == 100 ||
+                 gamesPlayed == 1000 )
+            {
+                lstGameLog.Items.Add("*** You've tried your luck " + gamesPlayed + " times! ***");
+            }
+        }
+
+        // Reset game button
+
+        private void btnResetGame_Click(object sender, EventArgs e)
+        {
+            DialogResult wantsToReset = MessageBox.Show(
+                "Are you sure you want to reset the game?\r\nYour win/loss history will be lost!",
+                "Confirm reset",
+                MessageBoxButtons.OKCancel);
+            if (wantsToReset == DialogResult.OK) {
+            ClearLog();
+            // Reset wins and losses values
+            wins = losses = 0;
+            // Revert label text
+            lblCounterWins.Text = "Wins: 0";
+            lblCounterLosses.Text = "Losses: 0";
+            lblWinLossPercentage.Text = "W / L: 0%";
+            }
+        }
+    }
+}
